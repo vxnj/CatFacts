c88e96bc77b9f0aa9cbd8236c90d9dd558b18278
vxnj <94234012+vxnj@users.noreply.github.com>
2021-12-18 20:59:33 -0500

    a

--- funcs.js
+++ funcs.js
@@ -1,14 +1,18 @@
 
-function validate(age){
+/* function validate(age){
     var ans="not eigible";
     if(age>=18){
         ans="eligible";
     }
     return(ans);
-
 }
+*/
 
-function hhmm(UNIX_timestamp){
-    var a = Date(UNIX_timestamp * 1000);
-    return(a.getHours() + ":" + a.getMinutes());
+
+function hhmm(dt){
+    var hour = dt.getHours();
+    var min = dt.getMinutes();
+    var time = hour + ':' + min;
+    return(time);
+
 }
