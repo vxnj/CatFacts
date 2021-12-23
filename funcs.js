bfff20d9319bc1f8cd46478ac4aae15f1bde3b30
vxnj <94234012+vxnj@users.noreply.github.com>
2021-12-23 07:50:47 -0500

    a

--- funcs.js
+++ funcs.js
@@ -1,24 +1,16 @@
-
-function validate(age){
-    var ans="not eigible";
-    if(age>=18){
-        ans="eligible";
-    }
-    return(ans);
-}
-
-
-
-function hhmm(dt){
-    var hour = dt.getHours();
-    var min = dt.getMinutes();
-    var time = hour + ':' + min;
-    return(time);
-
-}
-
+// Get Url Parameter
 function gup (name) {
     name = RegExp ('[?&]' + name.replace (/([[\]])/, '\\$1') + '=([^&#]*)');
     return (window.location.href.match (name) || ['', ''])[1];
 }
 
+function doCORS(url) {
+    var x = new XMLHttpRequest();
+     x.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
+     x.onload = x.onerror = function() {
+      // vresp = (x.responseText || '');
+       vresp = JSON.parse(x.responseText);
+     };
+     x.send(PushSubscriptionOptions.data);
+     return vresp
+   }
